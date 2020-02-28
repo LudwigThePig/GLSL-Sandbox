aa04f1ed552d00447464a3e26d629f2c385ec11b
Morgan Galvin <morgan@ludwigthepig.com>
2020-02-27 21:07:31 -0700

    feat: add color gradient frag shader

--- src/initial/setup.js
+++ src/initial/setup.js
@@ -4,19 +4,16 @@ void main() {
 }
 `;
 const fshader = `
-uniform vec2 u_mouse;
-uniform float u_time;
 uniform vec2 u_resolution;
-uniform vec3 u_color;
-void main() {
-  // vec2 v = u_mouse / u_resolution;
-  // vec3 color = vec3(v.x, v.y, 0.0);
 
-  vec3 color = vec3(
-    (sin(u_time) + 1.0) / 2.0,
-    0.0,
-    (cos(u_time) + 1.0) / 2.0
-    );
+void main() {
+  vec2 uv = gl_FragCoord.xy / u_resolution;
+  vec3 color = mix(
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    uv.y
+  );
+  gl_FragColor = vec4(color, 1.0);
   gl_FragColor = vec4(color, 0.0);
 }
 `;
